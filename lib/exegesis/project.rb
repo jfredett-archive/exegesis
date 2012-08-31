52cd02d2d5b6b798feb1f923c941b98780d82844
Joe Fredette <jfredett@gmail.com>
2012-08-30 22:04:03 -0400

    document Project

--- lib/exegesis/project.rb
+++ lib/exegesis/project.rb
@@ -1,4 +1,9 @@
+# Encapsulates the root directory of a project
 class Project
+
+  # @param root [String] the root path of the project
+  # @param searcher [FileSearcher] an object used to search for files in a given
+  # directory
   def initialize(root, searcher = Dir)
     @root = root
     @searcher = searcher
@@ -6,10 +11,12 @@ class Project
 
   attr_reader :root
 
+  # All of the directories in the root of the project
   def directories
     content.select { |s| File.directory?(s) }.map { |s| File.basename(s) }
   end
 
+  # All of the files in the root of the project
   def files
     content.reject { |s| File.directory?(s) }.map { |s| File.basename(s) } 
   end
