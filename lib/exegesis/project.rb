a4eb07547d49afc3e41879da23d7586be6d030d6
Joe Fredette <jfredett@gmail.com>
2012-08-30 22:34:33 -0400

    refactor Project to rely on an abstract FileSearcher

--- lib/exegesis/project.rb
+++ lib/exegesis/project.rb
@@ -1,31 +1,24 @@
 # Encapsulates the root directory of a project
 class Project
+  extend Forwardable
+
+  attr_reader :root
 
   # @param root [String] the root path of the project
   # @param searcher [FileSearcher] an object used to search for files in a given
   # directory
-  def initialize(root, searcher = Dir)
+  def initialize(root, searcher)
     @root = root
-    @searcher = searcher
+    @searcher = searcher.new
   end
 
-  attr_reader :root
-
-  # All of the directories in the root of the project
-  def directories
-    content.select { |s| File.directory?(s) }.map { |s| File.basename(s) }
-  end
-
-  # All of the files in the root of the project
-  def files
-    content.reject { |s| File.directory?(s) }.map { |s| File.basename(s) } 
-  end
+  delegate [:directories, :files] => :content
 
   private
 
   attr_reader :searcher
 
   def content
-    searcher[root + '*']
+    searcher.search(root + '*')
   end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Project do
   let (:root) { double('some path to a project directory').as_null_object } 
-  let (:searcher) { double('directory searcher like Dir') } 
+  let (:searcher) { double('directory searcher like FileSearcher') } 
   let (:dir) { double('fake directory') }
   let (:file) { double('fake file') }
 
@@ -11,49 +11,60 @@ describe Project do
 
   before do
     searcher.stub(:[]).with(root + '*').and_return([dir, file])
-
-    File.stub(:directory?).with(dir).and_return(true)
-    File.stub(:directory?).with(file).and_return(false)
-
-    File.stub(:basename).with(dir).and_return(dir)
-    File.stub(:basename).with(file).and_return(file)
+    searcher.stub(:new).and_return(searcher)
+    searcher.stub(:search).and_return(searcher)
   end
 
-  subject { project } 
+  describe 'initialization' do
+    before {
+      searcher.stub!(:new)
+      Project.new(root, searcher)
+    }
 
-  describe 'api' do
-    it { should respond_to :root }
-    it { should respond_to :directories } 
-    it { should respond_to :files } 
+    the(:searcher) { should have_received :new }
   end
 
-  describe '#root' do
-    subject { project.root } 
+  context do
+    subject { project } 
 
-    it { should == root }
+    describe 'api' do
+      it { should respond_to :root }
+      it { should respond_to :directories } 
+      it { should respond_to :files } 
+    end
+
+    describe '#root' do
+      subject { project.root } 
+      it { should == root }
+    end
+
+    describe '#directories' do
+      before { 
+        searcher.stub(:directories) 
+        project.directories
+      } 
+
+      the(:searcher) { should have_received :directories  } 
+    end
+
+    describe '#files' do 
+      before { 
+        searcher.stub(:files) 
+        project.files
+      } 
+
+      the(:searcher) { should have_received :files } 
+
+      #it { should delegate(:directories).to(:searcher).when_calling { project.directories } }
+      #it { should delegate(:directories).to(:searcher) } 
+    end
+
+    #it should also provide interface for
+    #  * creating files/directories
+    #    - something like `directories << Directory('foo')` ?
+    #
+    #  * 
+    #
+    #it should also have a #visit method, see the NOTES doc for details on that
   end
-
-  describe '#directories' do
-    subject { project.directories } 
-
-    let (:dirs) { [ dir ] }
-
-    it { should =~ dirs } 
-  end
-
-  describe '#files' do 
-    subject { project.files } 
-
-    let (:files) { [ file ] } 
-
-    it { should =~ files } 
-  end
-
-  #it should also provide interface for
-  #  * creating files/directories
-  #    - something like `directories << Directory('foo')` ?
-  #
-  #  * 
-  #
-  #it should also have a #visit method, see the NOTES doc for details on that
 end
