0399086f3033ef1303ea3762ffa08c30f548297e
Joe Fredette <jfredett@gmail.com>
2012-08-31 17:41:03 -0400

    document Exegesis toplevel

--- lib/exegesis.rb
+++ lib/exegesis.rb
@@ -7,3 +7,17 @@ require 'exegesis/file_searcher'
 require 'exegesis/directory'
 require 'exegesis/project'
 
+# Exegesis is a tool for automating many parts of the development of C projects.
+# Following a convention-over-configuration model.
+# 
+# It provides tools to: 
+# 
+#   * Create skeleton projects
+#   * Build
+#   * Testing
+#   * Packaging
+# 
+# Though C is presently the only supported language, it is the aim of Exegesis to
+# support tooling for other compiled languages.
+module Exegesis; end
+
