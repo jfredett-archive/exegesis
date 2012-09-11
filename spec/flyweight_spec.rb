4ad956896f30fc518ac9bc673e2e448d1e87f0bf
Joe Fredette <jfredett@gmail.com>
2012-09-11 08:04:50 -0400

    refactor flyweight specs

--- spec/flyweight_spec.rb
+++ spec/flyweight_spec.rb
@@ -10,7 +10,27 @@ describe Flyweight do
 
   before do
     processor.stub(:call).and_return(instance_key)
-    flyweight.register!(instance) 
+  end
+
+  context 'shared examples' do
+    shared_examples_for 'flyweight unregistration' do
+      before { flyweight.unregister!(key) } 
+
+      it { should_not have_key instance_key } 
+      it { should_not have_key instance }
+
+      describe 'unregistering an unused key' do
+        it 'raises an error' do
+          expect { flyweight.unregister!(key) }.to raise_error Flyweight::NoEntryError
+        end
+
+        describe '#unregister' do
+          it 'raises an error' do
+            expect { flyweight.unregister(key) }.to_not raise_error Flyweight::NoEntryError 
+          end
+        end
+      end
+    end
   end
 
   describe 'api' do
@@ -24,72 +44,48 @@ describe Flyweight do
     it { should respond_to :reset! } 
   end
 
-  describe '#register!' do
-    the(:processor) { should have_received(:call).with(instance) } 
+  describe 'implementation' do
+    before { flyweight.register!(instance) } 
 
-    describe 'registering an instance with an already-used key' do
-      it 'raises an error' do
-        expect { flyweight.register!(instance) }.to raise_error Flyweight::AlreadyRegisteredError
-      end
+    describe '#register! and #register' do
+      the(:processor) { should have_received(:call).with(instance) } 
 
-      describe '#register' do
-        it 'does not raise an error' do
-          expect { flyweight.register(instance) }.to_not raise_error Flyweight::AlreadyRegisteredError
-        end
-      end
-    end
-  end
-
-  describe '#[]' do
-    context 'by instance' do
-      subject { flyweight[instance] }
-
-      it { should be instance }
-    end
-
-    context 'by key' do
-      subject { flyweight[instance_key] }
-
-      it { should be instance } 
-    end
-  end
-
-  describe '#unregister!' do
-    context 'by key' do
-      before { flyweight.unregister!(instance_key) } 
-
-      it { should_not have_key instance_key } 
-      it { should_not have_key instance }
-
-      describe 'unregistering an unused key' do
+      describe 'registering an instance with an already-used key' do
         it 'raises an error' do
-          expect { flyweight.unregister!(instance_key) }.to raise_error Flyweight::NoEntryError
+          expect { flyweight.register!(instance) }.to raise_error Flyweight::AlreadyRegisteredError
         end
 
-        describe '#unregister' do
-          it 'raises an error' do
-            expect { flyweight.unregister(instance_key) }.to_not raise_error Flyweight::NoEntryError 
+        describe '#register' do
+          it 'does not raise an error' do
+            expect { flyweight.register(instance) }.to_not raise_error Flyweight::AlreadyRegisteredError
           end
         end
       end
     end
 
-    context 'by instance' do
-      before { flyweight.unregister!(instance) } 
+    describe '#[]' do
+      context 'by instance' do
+        subject { flyweight[instance] }
 
-      it { should_not have_key instance_key } 
-      it { should_not have_key instance }
+        it { should be instance }
+      end
 
-      describe 'unregistering an unused key' do
-        it 'raises an error' do
-          expect { flyweight.unregister!(instance) }.to raise_error Flyweight::NoEntryError
-        end
+      context 'by key' do
+        subject { flyweight[instance_key] }
 
-        describe '#unregister' do
-          it 'raises an error' do
-            expect { flyweight.unregister(instance) }.to_not raise_error Flyweight::NoEntryError
-          end
-        end
+        it { should be instance } 
+      end
+    end
+
+    describe '#unregister!' do
+      context 'by key' do
+        let (:key) { instance_key } 
+        it_behaves_like 'flyweight unregistration'
+      end
+
+      context 'by instance' do
+        let (:key) { instance } 
+        it_behaves_like 'flyweight unregistration'
       end
     end
   end
