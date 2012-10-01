10dfca1285e3a8650d0bced41fdf5e14b5e35aec
Joe Fredette <jfredett@gmail.com>
2012-10-01 18:04:55 -0400

    organize loader file.

--- lib/exegesis.rb
+++ lib/exegesis.rb
@@ -1,25 +1,43 @@
+
+################################################################################
+#load order matters here. These are sorted into load-levels Level-0 files must
+#be loaded before Level-1's, etc. try to keep this sorted. any file after the
+#line is 'load-whenever'
+################################################################################
+
+#level 0
 require 'rake'
 require 'rake/ext/string'
 
-require 'exegesis/version'
-
+#Level 1
 require 'exegesis/flyweight'
+
+#Level 2
+require 'exegesis/registerable'
+
+#Level 3
 require 'exegesis/file_searcher'
 require 'exegesis/directory'
 require 'exegesis/project'
 require 'exegesis/source_file'
 
+#-------------------------------------------------------------------------------
+#load whenever
+require 'exegesis/version'
+
+################################################################################
+
+
 # Exegesis is a tool for automating many parts of the development of C projects.
 # Following a convention-over-configuration model.
-# 
-# It provides tools to: 
-# 
+#
+# It provides tools to:
+#
 #   * Create skeleton projects
 #   * Build
 #   * Testing
 #   * Packaging
-# 
+#
 # Though C is presently the only supported language, it is the aim of Exegesis to
 # support tooling for other compiled languages.
 module Exegesis; end
-
