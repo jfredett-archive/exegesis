a446f5ee9ab6e5d42868a4bf27a8e825a69968f6
Joe Fredette <jfredett@gmail.com>
2012-11-14 08:36:35 -0500

    refactor and use a macro for defining child nodes.

--- lib/exegesis/project_ast/node.rb
+++ lib/exegesis/project_ast/node.rb
@@ -1,53 +1,82 @@
 # Represents a single node in the AST of a project structure
 module AST
   module Node
-    def visit(&block)
-      block.call(self)
+    def self.included(base)
+      base.send(:extend, ClassMethods)
+      base.send(:include, InstanceMethods)
+      base.send(:include, Enumerable)
+    end
 
-      children.each do |c|
-        c.visit(&block)
+    module InstanceMethods
+      def visit(&block)
+        block.call(self)
+
+        children.each do |c|
+          c.visit(&block)
+        end
+
+        nil
       end
 
-      nil
-    end
-  end
-
-  class Project
-    include Node
-
-    attr_reader :name
-    attr_reader :children
-
-    def initialize(name, &block)
-      @name = name
-      @children = []
-      instance_eval &block
+      def run
+      end
     end
 
-    def structure(&block)
-      children << Structure.new(&block)
-    end
+    module ClassMethods
+      def child_node(type)
+        define_method(type_to_method_name(type)) do |&block|
+          if @children.has_key?(type) and @children[type]
+            @children[type].run(&block)
+          else
+            @children[type] = type.new(&block)
+          end
+          @children[type]
+        end
+      end
 
-    def products(&block)
-      children << Products.new(&block)
-    end
+      private
 
-    def dependencies(&block)
-      children << Dependencies.new(&block)
+      def type_to_method_name(type)
+        type.to_s.split('::').last.downcase
+      end
     end
   end
 
   class Structure
     include Node
   end
+
   class Products
     include Node
   end
+
   class Dependencies
     include Node
   end
 
-  def project(name, &block) 
+  class Project
+    include AST::Node
+
+    attr_reader :name
+    attr_reader :children
+
+    def initialize(name, &block)
+      @name = name
+      @children = {}
+      instance_eval &block
+    end
+
+    def each(&block)
+      children.values.each(&block)
+    end
+
+    child_node Structure
+    child_node Products
+    child_node Dependencies
+  end
+
+
+  def project(name, &block)
     Project.new(name, &block)
   end
 end
--- spec/unit/project_ast/node_spec.rb
+++ spec/unit/project_ast/node_spec.rb
@@ -1,10 +1,49 @@
 require 'unit_spec_helper'
 
+# The tests for this stuff is not the greatest in the world, but the
+# transformations it does are relatively dumb, so I'm relying on
+# proof-by-inspection a bit. It's unfortunate, but there's really only
+# one way to build the internal DSL, and no good way to make assertions
+# around how it's parsing stuff.
+#
+# NOTE: one option would to lazily parse each component, that gives
+# a chance to make explicit the 'run' phase, so that message assertions
+# at least can make it in.
+#
+# NOTE: Another option is to have parsing emit events to another object
+# which can allow us to monitor them. We can then use that to look at a
+# sort of 'live trace' of each event, and make assertions about what's
+# happening. Eg.
+#
+#     project name:foo
+#       structure
+#         file location:baz type:markdown
+#         dir location:bar type:src
+#       dependencies:parse
+#         compiler name:clang options:'-Wall -Werror' supports:[.c, .cpp]
+#       structure:merge
+#         file location:README type:plain
+#         dir location:foo type:plain
+#           file:nested location:foo type:markdown 
+#       products:parse
+#         #.. snip
+#
+# This event trace would not be indented as above, but flat, however, each
+# trace would be predicticable and deterministic, so we could use it as
+# a way to ensure the parse went as expected.
+
 shared_examples_for 'an ast node' do
   describe 'AST Node API' do
     it { should respond_to :visit }
     it { should respond_to :children }
-    its(:children) { should respond_to :each }
+    it { should respond_to :run }
+    it { should respond_to :each }
+
+    its(:class) { should respond_to :child_node }
+    its(:children) { should respond_to :[] }
+    its(:children) { should respond_to :[]= }
+    its(:children) { should respond_to :has_key? }
+
   end
 end
 
@@ -22,6 +61,10 @@ describe AST::Project do
     it { should respond_to :structure    }
     it { should respond_to :products     }
     it { should respond_to :dependencies }
+
+    its(:structure)    { should be_a_kind_of AST::Structure    }
+    its(:products)     { should be_a_kind_of AST::Products     }
+    its(:dependencies) { should be_a_kind_of AST::Dependencies }
   end
 
   describe '#structure' do
@@ -32,7 +75,19 @@ describe AST::Project do
           end
         end
       }.to_not raise_error
-  end
+    end
+
+    it 'parses overspecified structure calls' do
+      expect {
+        project 'foo' do
+          structure do
+          end
+
+          structure do
+          end
+        end
+      }.to_not raise_error
+    end
   end
 
   describe '#products' do
@@ -44,12 +99,36 @@ describe AST::Project do
         end
       }.to_not raise_error
     end
+
+    it 'parses overspecified products calls' do
+      expect {
+        project 'foo' do
+          products do
+          end
+
+          products do
+          end
+        end
+      }.to_not raise_error
+    end
   end
 
   describe '#dependencies' do
     it 'parses #dependencies calls' do
       expect {
         project 'foo' do
+          dependencies do
+          end
+        end
+      }.to_not raise_error
+    end
+
+    it 'parses overspecified dependencies calls' do
+      expect {
+        project 'foo' do
+          dependencies do
+          end
+
           dependencies do
           end
         end
