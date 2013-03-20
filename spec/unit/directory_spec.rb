6cb8929f8d4e02d451fe620c2d70b64611bbe68f
Joe Fredette <jfredett@gmail.com>
2013-03-20 07:47:57 -0400

    implement Directory#find_dir and Directory#find_file

--- spec/unit/directory_spec.rb
+++ spec/unit/directory_spec.rb
@@ -19,12 +19,13 @@ describe Directory do
   it_should_behave_like 'a FileSystemEntity'
 
   describe 'api' do
-    it { should respond_to :directories }
-    it { should respond_to :parent      }
-    it { should respond_to :files       }
-    it { should respond_to :path        }
-    it { should respond_to :children    }
-    it { should respond_to :[]          }
+    it { should respond_to :directories    }
+    it { should respond_to :parent         }
+    it { should respond_to :files          }
+    it { should respond_to :path           }
+    it { should respond_to :children       }
+    it { should respond_to :find_file      }
+    it { should respond_to :find_directory }
 
     its(:class) { should respond_to :create }
   end
@@ -38,4 +39,29 @@ describe Directory do
     it { should be_a String }
     it { should == File.join(parent_path, name) }
   end
+
+  describe '#find_file' do
+    let(:file_foo) { double("sourcefile('foo')", name: 'foo') }
+    let(:file_bar) { double("sourcefile('bar')", name: 'bar') }
+    let(:files) { [file_foo, file_bar] }
+
+    before { directory.stub(:files).and_return files }
+
+    subject { directory.find_file('foo') }
+
+    it { should be file_foo }
+  end
+
+  describe '#find_directory' do
+    let(:directory_foo) { double("directory('foo')", name: 'foo') }
+    let(:directory_bar) { double("directory('bar')", name: 'bar') }
+    let(:directories) { [directory_foo, directory_bar] }
+
+    before { directory.stub(:directories).and_return directories }
+
+    subject { directory.find_directory('foo') }
+
+    it { should be directory_foo }
+  end
+
 end
